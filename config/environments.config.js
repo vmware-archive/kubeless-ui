33bf78546ba5ecdd8f2ca591281f5759940679d5
kemcake <santos.remi@icloud.com>
2017-11-04 14:34:47 +0800

    Update compiler_public_path to relative #41

--- config/environments.config.js
+++ config/environments.config.js
@@ -16,7 +16,7 @@ module.exports = {
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   production : (config) => ({
-    compiler_public_path     : '/',
+    compiler_public_path     : '',
     compiler_fail_on_warning : false,
     compiler_hash_type       : 'chunkhash',
     compiler_devtool         : false,
